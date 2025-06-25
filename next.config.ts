8d0e306971529ea9ea6df5fe1f02d2b49a34b365
AbhiPurohit1100 <abhinavpurohit30@gmail.com>
2025-06-25 11:55:19 +0530

    last

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images:{
+    unoptimized: true
+  }, 
+  basePath : '',
+  assetPrefix: './',
+  trailingSlash : true
   /* config options here */
 };
 
